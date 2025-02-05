150c73c6db1402841801a58dd965fa656555bba4
Lucas Leal <leallucasfonseca@gmail.com>
2025-02-05 20:32:52 -0300

    config: update sqs.tf

--- infra/sqs.tf
+++ infra/sqs.tf
@@ -1,15 +1,9 @@
 # Configuração da Fila SQS processar
 resource "aws_sqs_queue" "processar" {
   name                      = "processar"
-  visibility_timeout_seconds = 300
-  message_retention_seconds = 86400
-  delay_seconds             = 10
 }
 
 # Configuração da Fila SQS processados
 resource "aws_sqs_queue" "processados" {
   name                      = "processados"
-  visibility_timeout_seconds = 300
-  message_retention_seconds = 86400
-  delay_seconds             = 10
 }
